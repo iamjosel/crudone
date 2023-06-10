714f84bed4d23b378e46cede41d2e899c592c2e6
iamjosel <iamjoseluisg@gmail.com>
2023-06-10 17:29:22 -0500

    funcionando el botón de agregar

--- crud/libreria/urls.py
+++ crud/libreria/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.inicio,name='inicio'),
     path('about', views.about, name='about'),
     path('books', views.books, name='books'),
+    path('books/create', views.create_book, name='create_book'),
 ]
