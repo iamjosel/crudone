a2ef73e9f673b1242ec3a49aabd4934f6d08df39
iamjosel <iamjoseluisg@gmail.com>
2023-06-10 17:05:24 -0500

    avance hasta las plantillas, funciona la navegación

---
+++ crud/libreria/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+def libros(request):
+    return render(request, 'libros/index.html')
