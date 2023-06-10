6fe398e2356322a30b40163956813cf45ad9bda1
iamjosel <iamjoseluisg@gmail.com>
2023-06-10 18:42:52 -0500

    migración y creación data bases

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'crud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'libreria',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- crud/libreria/models.py
+++ crud/libreria/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length=100, verbose_name = 'Título')
+    image = models.ImageField(upload_to='images/', verbose_name = 'Imagen', null = True)
+    description = models.TextField(verbose_name = 'Descripción', null=True)
